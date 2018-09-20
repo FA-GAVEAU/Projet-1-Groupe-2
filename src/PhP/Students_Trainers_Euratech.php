f68992b3de1f4d49dc2352fa2cf7e283546eacb4
François-Axel GAVEAU <gaveau.francois-axel@orange.fr>
2018-09-20 15:21:38 +0200

    Update Students_Trainers_Euratech.php

--- src/PhP/Students_Trainers_Euratech.php
+++ src/PhP/Students_Trainers_Euratech.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="Students-Trainers-Euratech.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat+Alternates" rel="stylesheet">
     <title>Pleasures of Wild !</title>
 </head>

525be52f0a35391171251d4dd2c3c1216e34c63b
Christophe Kuchmac <christophekuchmac@free.fr>
2018-09-20 21:29:59 +0200

    Update footer.inc.php
    New release footer with links

--- src/PhP/footer.inc.php
+++ src/PhP/footer.inc.php
@@ -1,75 +1,24 @@
 <!-- Footer -->
-<footer class="page-footer font-small blue pt-4">
-
-    <!-- Footer Links -->
-    <div class="container-fluid text-center text-md-left">
-
-        <!-- Grid row -->
-        <div class="row">
-
-            <!-- Grid column -->
-            <div class="col-md-6 mt-md-0 mt-3">
-
-                <!-- Content -->
-                <h5 class="text-uppercase">Footer Content</h5>
-                <p>Here you can use rows and columns here to organize your footer content.</p>
-
-            </div>
-            <!-- Grid column -->
-
-            <hr class="clearfix w-100 d-md-none pb-3">
-
-            <!-- Grid column -->
-            <div class="col-md-3 mb-md-0 mb-3">
-
-                <!-- Links -->
-                <h5 class="text-uppercase">Links</h5>
-
-                <ul class="list-unstyled">
-                    <li>
-                        <a href="#!">Link 1</a>
-                    </li>
-                    <li>
-                        <a href="#!">Link 2</a>
-                    </li>
-                    <li>
-                        <a href="#!">Link 3</a>
-                    </li>
-                    <li>
-                        <a href="#!">Link 4</a>
-                    </li>
-                </ul>
-
-            </div>
-            <!-- Grid column -->
-
-            <!-- Grid column -->
-            <div class="col-md-3 mb-md-0 mb-3">
-
-                <!-- Links -->
-                <h5 class="text-uppercase">Links</h5>
-
-                <ul class="list-unstyled">
-                    <li>
-                        <a href="#!">Link 1</a>
-                    </li>
-                    <li>
-                        <a href="#!">Link 2</a>
-                    </li>
-                    <li>
-                        <a href="#!">Link 3</a>
-                    </li>
-                    <li>
-                        <a href="#!">Link 4</a>
-                    </li>
-                </ul>
-
-            </div>
-            <!-- Grid column -->
-
-        </div>
-        <!-- Grid row -->
-
-    </div>
-    <!-- Footer Links -->
+<footer>
+<nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-center">
+    <a class="navbar-brand" href="https://www.lagendarmerierecrute.fr/">Join Us</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="http://usr3-nein.ch/wp-content/uploads/2016/11/non-1.png">Are You ready ? <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="https://pbs.twimg.com/profile_images/487014630762745856/cS1NeBQh.jpeg">PHP effect</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="http://ekladata.com/MZcjnHU8BWKUw5YJDBhMHeiF6dQ.jpg">JS effect</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="http://ekladata.com/4pRKJKISZJAJKDM_1-J5iJh8sEA@550x412.jpg">Founder</a>
+            </li>
+        </ul>
+        <img src="41307141_298501020929136_4412821525019754496_n.jpg" height="10%" width="10%" alt="logo">
+</nav>
 </footer>

6a14991e2b88ba1ddb299a79cb0c71f2f3825777
Elie Delattre <42995492+silomonochrome@users.noreply.github.com>
2018-09-20 16:20:23 +0100

    Update Home.php

--- src/PhP/Home.php
+++ src/PhP/Home.php
@@ -9,7 +9,6 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="style.js">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300|Ramabhadra" rel="stylesheet">
     <title>Pleasures of Wild !</title>
 </head>
